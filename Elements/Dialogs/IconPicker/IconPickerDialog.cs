4a7e9db6e0cfc44ba7e65b134b269c6d1e5e13a3
DivineBytes <Admin@DivineBytes>
2023-02-25 17:19:16 -0600

    Update

--- Elements/Dialogs/IconPicker/IconPickerDialog.cs
+++ Elements/Dialogs/IconPicker/IconPickerDialog.cs
@@ -7,20 +7,49 @@ using System.Windows.Forms;
 
 namespace Elements.Dialogs
 {
+    /// <summary>
+    /// The <see cref="IconPickerDialog"/> class.
+    /// </summary>
+    /// <seealso cref="System.Windows.Forms.CommonDialog" />
     public class IconPickerDialog : CommonDialog
     {
+        /// <summary>
+        /// Shes the pick icon dialog.
+        /// </summary>
+        /// <param name="hWnd">The h WND.</param>
+        /// <param name="pszFilename">The PSZ filename.</param>
+        /// <param name="cchFilenameMax">The CCH filename maximum.</param>
+        /// <param name="pnIconIndex">Index of the pn icon.</param>
+        /// <returns></returns>
         [DllImport("shell32.dll", EntryPoint = "#62", CharSet = CharSet.Unicode, SetLastError = true)]
         [SuppressUnmanagedCodeSecurity]
         public static extern bool SHPickIconDialog(IntPtr hWnd, StringBuilder pszFilename, int cchFilenameMax, out int pnIconIndex);
 
         private const int MAX_PATH = 260;
 
+        /// <summary>
+        /// Gets or sets the name of the file.
+        /// </summary>
+        /// <value>
+        /// The name of the file.
+        /// </value>
         [DefaultValue(default(string))]
         public string FileName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the index of the icon.
+        /// </summary>
+        /// <value>
+        /// The index of the icon.
+        /// </value>
         [DefaultValue(0)]
         public int IconIndex { get; set; }
 
+        /// <summary>
+        /// Runs the dialog.
+        /// </summary>
+        /// <param name="hwndOwner">The HWND owner.</param>
+        /// <returns></returns>
         protected override bool RunDialog(IntPtr hwndOwner)
         {
             var buf = new StringBuilder(FileName, MAX_PATH);
@@ -36,6 +65,9 @@ namespace Elements.Dialogs
             return ok;
         }
 
+        /// <summary>
+        /// Resets this instance.
+        /// </summary>
         public override void Reset()
         {
             FileName = null;
