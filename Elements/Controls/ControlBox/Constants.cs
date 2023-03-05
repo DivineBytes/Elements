5e9b945c866b5d8db845b78bb012b95e0eee6453
DivineBytes <Admin@DivineBytes>
2023-03-05 14:06:39 -0600

    Update

---
+++ Elements/Controls/ControlBox/Constants.cs
@@ -0,0 +1,33 @@
+﻿namespace Elements.Controls.ControlBox
+{
+    /// <summary>
+    /// The <see cref="Constants"/> class.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// The Close.
+        /// </summary>
+        public const char Close = 'r';
+
+        /// <summary>
+        /// The Help.
+        /// </summary>
+        public const char Help = 's';
+
+        /// <summary>
+        /// The Maximize.
+        /// </summary>
+        public const char Maximize = '1';
+
+        /// <summary>
+        /// The Minimize.
+        /// </summary>
+        public const char Minimize = '0';
+
+        /// <summary>
+        /// The Restore.
+        /// </summary>
+        public const char Restore = '2';
+    }
+}
